9a76b9310443ee97622b41dd1047bdd24b10b865
Trey <ctrkwang1@gmail.com>
2018-10-24 18:51:44 +0900

    Bubble Sort in Python

--- Sorts/bubbleSort.py
+++ Sorts/bubbleSort.py
@@ -5,10 +5,15 @@ if one is bigger than another.
 '''
 
 def bubbleSort(unsorted_list):
-    
+    _length = len(unsorted_list)
 
+    for i in range(_length):
+        for j in range(0, _length-i-1):
+            if unsorted_list[j] > unsorted_list[j+1]:
+                unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
+    return unsorted_list
 
 if __name__ == "__main__":
-    unsorted_list = []
+    unsorted_list = [21, 23, 5, 46, 1, 4, 8, 96, 65, 85]  # Create a unsorted number list
     sorted_list = bubbleSort(unsorted_list)
     print(sorted_list)
