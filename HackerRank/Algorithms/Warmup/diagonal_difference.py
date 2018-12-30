a634ee998f351d8ddd5fb4bd761a84377f5e81da
TreyYi <ctrkwang1@gmail.com>
2018-12-31 02:20:43 +0900

    remove unnecessary comments

--- HackerRank/Algorithms/Warmup/diagonal_difference.py
+++ HackerRank/Algorithms/Warmup/diagonal_difference.py
@@ -1,5 +1,3 @@
-import math
-
 def diagonal_difference(arr):
     arr_len = len(arr)
     front_index = 0  #start point
@@ -7,10 +5,8 @@ def diagonal_difference(arr):
     result = []
     for i in range(arr_len):
         result.append(arr[i][front_index]-arr[i][back_index])
-        # print(arr[i][front_index], arr[i][back_index])
         front_index += 1
         back_index -= 1
-        # print("debug:", result)
     return abs(sum(result))
 
 if __name__ == "__main__":
